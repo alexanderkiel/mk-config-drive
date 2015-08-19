22a8fe4bce2f30b9509fa9daef72000093a56973
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-19 21:19:14 +0200

    Nicer Output

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ mkdir -p /new-drive/openstack/latest
 
 for f in /config/*.yml
 do
-	cp $f /new-drive/openstack/latest/user_data
-	mkisofs -R -V config-2 -input-charset utf-8 -o "/config/$(basename $f .yml).iso" /new-drive
+    echo "Build ISO for $(basename $f)"
+    cp $f /new-drive/openstack/latest/user_data
+    mkisofs -R -V config-2 -input-charset utf-8 -o "/config/$(basename $f .yml).iso" /new-drive
+    echo ""
 done
